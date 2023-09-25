deda28a1fb1fea97be60b924f457cef3adc55104
Rebecca Hemstad <{28883539}+{RHemstad}@users.noreply.github.com>
2023-09-25 16:09:19 -0400

    homepageWip

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -7,13 +7,17 @@ const Hero = () => {
   return (
     <>
 
-<section id="hero" className="hero">
+<section id="hero_wrapper" className="hero">
+
+<div>
 
 <p>Manage and keep track of your zombie data in a fun and interactive way with our Zombie Database Management System. Whether you're a zombie enthusiast or just curious, our application allows you to add, edit, and delete zombie records seamlessly.</p>
 
 <Link to="/dashboard">
 <Button className="primary-button">Get Started</Button>
 </Link>
+
+</div>
 
 </section>
     
