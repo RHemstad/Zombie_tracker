1ef2acb0875641534127064e6e8d289b7a405760
Rebecca Hemstad <{28883539}+{RHemstad}@users.noreply.github.com>
2023-10-03 20:27:34 -0400

    change link

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -18,7 +18,7 @@ id="hero_wrapper"
 
 <p>Manage and keep track of your zombie data in a fun and interactive way with our Zombie Database Management System. {/*Whether you're a zombie enthusiast or just curious, our application allows you to add, edit, and delete zombie records seamlessly.*/}</p>
 
-<Link to="/login">
+<Link to="/register">
 <Button className="primary-button">Get Started</Button>
 </Link>
  
